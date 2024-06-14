d4fcdfb8763531fa34d09a122288a4883907cb4b
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-14 16:23:07 +0530

    Initialize

---
+++ vaadin-app/src/main/java/org/example/vaadinapp/VaadinAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.vaadinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinAppApplication.class, args);
+	}
+
+}
