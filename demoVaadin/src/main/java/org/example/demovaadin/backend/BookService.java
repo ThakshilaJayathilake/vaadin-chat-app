dbaab4cdf87f1ab1aa697d59ce10f3a50623efc6
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-16 19:06:16 +0530

    Implement backend with JPA

---
+++ demoVaadin/src/main/java/org/example/demovaadin/backend/BookService.java
@@ -0,0 +1,32 @@
+package org.example.demovaadin.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.vaadin.crudui.crud.CrudListener;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class BookService implements CrudListener<Book> {
+    private final BookRepository bookRepository;
+    @Override
+    public Collection<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book add(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Book update(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+}
