6639246d27446408aaba2ca1f92774506f853aeb
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-16 23:43:30 +0530

    added comments

--- demoVaadin/src/main/java/org/example/demovaadin/DemoVaadinApplication.java
+++ demoVaadin/src/main/java/org/example/demovaadin/DemoVaadinApplication.java
@@ -10,18 +10,41 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+//class extends VaadinWebSecurity, which is a helper class provided by Vaadin
+// to integrate Vaadin applications with Spring Security
 @SpringBootApplication
 public class DemoVaadinApplication extends VaadinWebSecurity {
 
+//	starting the embedded server and initializing Spring components
 	public static void main(String[] args) {
 		SpringApplication.run(DemoVaadinApplication.class, args);
 	}
 
+//	here, use Spring Security for authentication
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+//		call sets up the default configurations provided by VaadinWebSecurity
 		super.configure(http);
+//		specifies the LoginView class as the login page for the application
 		setLoginView(http, LoginView.class);
 	}
+
+//	used by Spring Security to load user-specific data
+//	@Bean
+//	public UserDetailsService userDetailsServiceBean() throws Exception {
+////		used to store user details in memory
+//		return new InMemoryUserDetailsManager(
+////				{noop} prefix indicates that the passwords are stored in plain text and no encoding is applied
+//				User.withUsername("tnj")
+//						.password("{noop}password")
+//						.roles("ADMIN")
+//						.build(),
+//				User.withUsername("Maria")
+//						.password("{noop}p")
+//						.roles("ADMIN")
+//						.build()
+//		);
+//	}
 
 	@Bean
 	public UserDetailsService userDetailsServiceBean() throws Exception {
@@ -36,4 +59,5 @@ public class DemoVaadinApplication extends VaadinWebSecurity {
 						.build()
 		);
 	}
+
 }
--- demoVaadin/src/main/java/org/example/demovaadin/UI/HomeView.java
+++ demoVaadin/src/main/java/org/example/demovaadin/UI/HomeView.java
@@ -5,11 +5,15 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
-
+//maps this view to the root URL path
 @Route("")
+// annotation allows unauthenticated (anonymous) users to access this view.
+// Without this annotation, access might be restricted based on security configurations
 @AnonymousAllowed
+// VerticalLayout is a Vaadin component that lays out its children vertically
 public class HomeView extends VerticalLayout {
     public HomeView(){
+//        adds an H1 (header) component to the layout
         add(
                 new H1("Home view")
         );
