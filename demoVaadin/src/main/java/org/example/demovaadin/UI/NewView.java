8bbbae601458c96e40595a5e40ec451c8b3443e7
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-17 20:51:16 +0530

    binding

--- demoVaadin/src/main/java/org/example/demovaadin/UI/NewView.java
+++ demoVaadin/src/main/java/org/example/demovaadin/UI/NewView.java
@@ -4,13 +4,15 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
-
-import java.awt.*;
+import org.example.demovaadin.backend.Book;
+import com.vaadin.flow.data.binder.Binder;
+import org.example.demovaadin.backend.BookService;
 
 @Route("new")
 @RolesAllowed("ADMIN")
@@ -22,11 +24,20 @@ public class NewView extends VerticalLayout {
     private IntegerField rating = new IntegerField("Rating");
 
 
-    public NewView(){
+    public NewView(BookService service){
+
+        var binder = new Binder<>(Book.class);
+        binder.bindInstanceFields(this);
         add(
                 new H1("New Book"),
                 new FormLayout(title, published, rating),
-                new Button("Save")
+                new Button("Save", event -> {
+                    var book = new Book();
+                    binder.writeBeanIfValid(book);
+                    service.add(book);
+                    Notification.show("Book saved");
+                    binder.readBean(new Book());
+                })
         );
     }
 }
